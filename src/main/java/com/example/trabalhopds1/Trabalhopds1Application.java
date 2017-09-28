d594fd4658536fff2e632219f8ae595d23a20929
mayllats <mayllats@gmail.com>
2017-09-28 20:40:45 -0300

    Projeto criado

---
+++ src/main/java/com/example/trabalhopds1/Trabalhopds1Application.java
@@ -0,0 +1,12 @@
+package com.example.trabalhopds1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalhopds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalhopds1Application.class, args);
+	}
+}

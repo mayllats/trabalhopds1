21c590d7f4010a62d9a7f2e72908f46d07a3d461
mayllats <mayllats@gmail.com>
2017-10-04 22:10:02 -0300

    Produto associacao muitos para muitos

---
+++ src/main/java/com/example/trabalhopds1/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.trabalhopds1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.trabalhopds1.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}

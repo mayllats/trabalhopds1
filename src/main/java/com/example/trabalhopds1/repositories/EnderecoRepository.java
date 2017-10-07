66bb25d89087bcd8ff5ea5d4b28425d77d56d84e
mayllats <mayllats@gmail.com>
2017-10-06 23:14:53 -0300

    Cliente, TipoCliente, telefones, enderecos

---
+++ src/main/java/com/example/trabalhopds1/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.example.trabalhopds1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.trabalhopds1.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}

d6ae85483296edd5f1f4398fdbc3e1e354118883
mayllats <mayllats@gmail.com>
2017-10-19 18:43:15 -0200

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/example/trabalhopds1/repositories/ClienteRepository.java
+++ src/main/java/com/example/trabalhopds1/repositories/ClienteRepository.java
@@ -1,11 +1,16 @@
 package com.example.trabalhopds1.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.trabalhopds1.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
